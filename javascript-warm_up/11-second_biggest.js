fd438d3919f0e2d0b01334be56202a805b2540fa
aylaismayilzada6 <aismayilzada19980@ada.edu.az>
2026-05-09 19:02:35 +0000

    tasks

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -5,8 +5,6 @@ if (args.length <= 1) {
   console.log(0);
 } else {
   const numbers = args.map(Number).sort((a, b) => b - a);
-  const uniqueNumbers = [...new Set(numbers)]; // Handle duplicates if necessary
-  
-  // If the prompt implies distinct values or just the second in sorted list:
+  const uniqueNumbers = [...new Set(numbers)];
   console.log(uniqueNumbers[1] || 0);
 }
