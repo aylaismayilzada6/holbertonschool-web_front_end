e0748ace74324dac6f2db33d31fedaa90faf82f5
aylaismayilzada6 <aismayilzada19980@ada.edu.az>
2026-05-09 18:50:03 +0000

    tasks

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-cont lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (const line in lines){
 	console.log(line)
